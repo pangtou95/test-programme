c87c89f30d76d353452ce15ed951acbc7d5af8c5
pangtou95 <157006855+pangtou95@users.noreply.github.com>
2026-05-13 13:32:18 +0900

    Update package export name

--- src/index.mjs
+++ src/index.mjs
@@ -1,2 +1,2 @@
-export const packageName = "universal-quality-gate";
+export const packageName = "test-programme";
 export const supportedTargets = ["web", "browser", "ios", "android", "desktop-app", "http"];
